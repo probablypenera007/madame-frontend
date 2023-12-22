6d2d627e503765df737e47214ad21e4cbc59b318
James Penera <jamespenera@icloud.com>
2023-12-22 06:13:05 -0800

    Update RegisterModal.js

--- src/components/RegisterModal/RegisterModal.js
+++ src/components/RegisterModal/RegisterModal.js
@@ -1,6 +1,6 @@
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import { useForm } from '../../hooks/useForm';
+import { useForm } from "../../hooks/useForm";
 import { useState } from "react";
 
 const RegisterModal = ({
@@ -10,40 +10,19 @@ const RegisterModal = ({
   buttonText,
   openLogInModal,
 }) => {
-  const { values, handleChange } = useForm({ email: '', password: '', name: '', avatar: '' });
+  const { values, handleChange } = useForm({
+    name: "",
+    dob: "",
+    email: "",
+    password: "",
+  });
 
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [name, setName] = useState("");
-  // const [avatar, setAvatar] = useState("");
   const history = useHistory();
-
-  // const handleEmailChange = (e) => {
-  //   setEmail(e.target.value);
-  // };
-
-  // const handlePasswordChange = (e) => {
-  //   setPassword(e.target.value);
-  // };
-
-  // const handleNameChange = (e) => {
-  //   setName(e.target.value);
-  // };
-
-  // const handleAvatarChange = (e) => {
-  //   setAvatar(e.target.value);
-  // };
-
-  // const handleFormSubmitRegister = (e) => {
-  //   e.preventDefault();
-  //   onSubmit({ email, password, name, avatar });
-  // };
 
   const handleFormSubmitRegister = (e) => {
     e.preventDefault();
     onSubmit(values);
   };
-  
 
   const handleOpenLogin = (e) => {
     e.preventDefault();
@@ -60,6 +39,33 @@ const RegisterModal = ({
         buttonText={buttonText}
         modalName={"Register_Modal"}
       >
+        <label className="modal__label modal__label_register">
+          Name
+          <input
+            id="register-name"
+            className="modal__input-text modal__input_text-register"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={values.name}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+        </label>
+        <label className="modal__label modal__label_register">
+          DOB*
+          <input
+            id="register-dob"
+            className="modal__input_text modal__input_text-register"
+            type="date"
+            name="dob"
+            placeholder="Date of Birth"
+            value={values.dob}
+            onChange={handleChange}
+            required
+          />
+        </label>
         <label className={"modal__label modal__label_register"}>
           Email*
           <input
@@ -70,7 +76,6 @@ const RegisterModal = ({
             placeholder="Email"
             value={values.email}
             onChange={handleChange}
-            // onChange={handleEmailChange}
             minLength="1"
             maxLength="30"
             autoComplete="off"
@@ -91,37 +96,6 @@ const RegisterModal = ({
             minLength="1"
             autoComplete="off"
             required
-          />
-        </label>
-        <label className="modal__label modal__label_register">
-          Name
-          <input
-            id="register-name"
-            className="modal__input-text modal__input_text-register"
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={values.name}
-            onChange={handleChange}
-            // onChange={handleNameChange}
-            autoComplete="off"
-            required
-          />
-        </label>
-        <label className="modal__label modal__label_register">
-          Avatar URL
-          <input
-            id="register-avatar"
-            className="modal__input-text modal__input_text-register"
-            type="url"
-            name="avatar"
-            placeholder="Avatar URL"
-            value={values.avatar}
-            onChange={handleChange}
-            // onChange={handleAvatarChange}
-            minLength="1"
-            autoComplete="off"
-            // required
           />
         </label>
         <Link to="/" className="login__link" onClick={handleOpenLogin}>
