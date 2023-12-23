beafde21f5336c77cbb037dc05898f1ae0bf8881
James Penera <jamespenera@icloud.com>
2023-12-22 22:13:34 -0800

    Create zodiac.js

---
+++ src/utils/zodiac.js
@@ -0,0 +1,59 @@
+const zodiacSign = [
+    {
+        name: 'Aries',
+        date: 'March 21 - April 19',
+        symbol: '♈',
+    },
+    {
+        name: 'Taurus',
+        date: 'April 20 - May 20',
+        symbol: '♉',
+    },
+    {
+        name: 'Gemini',
+        date: 'May 21 - June 20',
+        symbol: '♊',
+    },
+    {
+        name: 'Cancer',
+        date: 'June 21 - July 22',
+        symbol: '♋',
+    },
+    {
+        name: 'Leo',
+        date: 'July 23 - August 22',
+        symbol: '♌',
+    },
+    {
+        name: 'Virgo',
+        date: 'August 23 - September 22',
+        symbol: '♍',
+    },
+    {
+        name: 'Libra',
+        date: 'September 23 - October 22',
+        symbol: '♎',
+    },
+    {
+        name: 'Scorpio',
+        date: 'October 23 - November 21',
+        symbol: '♏',
+    },
+    {
+        name: 'Sagittarius',
+        date: 'November 22 - December 21',
+        symbol: '♐',
+    },
+    {
+        name: 'Capricorn',
+        date: 'December 22 - January 19',
+        symbol: '♑',
+    },
+    {
+        name: 'Aquarius',
+        date: 'January 20 - February 18',
+        symbol: '♒',
+    },
+]
+
+export default zodiacSign;
