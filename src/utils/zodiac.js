318e03bf7f2442721da3e2f0a89d5bd1987156ea
James Penera <jamespenera@icloud.com>
2023-12-26 06:34:13 -0800

    Update zodiac.js
    added images to symbol

--- src/utils/zodiac.js
+++ src/utils/zodiac.js
@@ -2,57 +2,57 @@ const zodiacSign = [
     {
         name: 'Aries',
         date: 'March 21 - April 19',
-        symbol: '♈',
+        symbol: require("../images/zodiac/aries-shadow.png").default,
     },
     {
         name: 'Taurus',
         date: 'April 20 - May 20',
-        symbol: '♉',
+        symbol: require("../images/zodiac/taurus-shadow.png").default,
     },
     {
         name: 'Gemini',
         date: 'May 21 - June 20',
-        symbol: '♊',
+        symbol: require("../images/zodiac/gemini-shadow.png").default,
     },
     {
         name: 'Cancer',
         date: 'June 21 - July 22',
-        symbol: '♋',
+        symbol: require("../images/zodiac/cancer-shadow.png").default,
     },
     {
         name: 'Leo',
         date: 'July 23 - August 22',
-        symbol: '♌',
+        symbol: require("../images/zodiac/leo-shadow.png").default,
     },
     {
         name: 'Virgo',
         date: 'August 23 - September 22',
-        symbol: '♍',
+        symbol: require("../images/zodiac/virgo-shadow.png").default,
     },
     {
         name: 'Libra',
         date: 'September 23 - October 22',
-        symbol: '♎',
+        symbol: require("../images/zodiac/libra-shadow.png").default,
     },
     {
         name: 'Scorpio',
         date: 'October 23 - November 21',
-        symbol: '♏',
+        symbol: require("../images/zodiac/scorpio-shadow.png").default,
     },
     {
         name: 'Sagittarius',
         date: 'November 22 - December 21',
-        symbol: '♐',
+        symbol: require("../images/zodiac/sagittarius-shadow.png").default,
     },
     {
         name: 'Capricorn',
         date: 'December 22 - January 19',
-        symbol: '♑',
+        symbol: require("../images/zodiac/capricorn-shadow.png").default,
     },
     {
         name: 'Aquarius',
         date: 'January 20 - February 18',
-        symbol: '♒',
+        symbol: require("../images/zodiac/aquarius-shadow.png").default,
     },
 ]
 
