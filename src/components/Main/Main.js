82250643c4be3bb10b392c38c0cfcdcd696cfbb5
James Penera <jamespenera@icloud.com>
2023-12-26 07:30:38 -0800

    added new font

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 import { zodiacSign } from "../../utils/zodiac";
+import { madameOracleQuestions } from "../../utils/MadameOraclePhrase";
 import { Link } from "react-router-dom";
 import "./Main.css";
 
-function Main({ weatherTemp, isDay }) {
+function Main({}) {
   const currentUser = useContext(CurrentUserContext);
-  const [currentZodiac, setCurrentZodiac] = useState({ name: '', symbol: '' })
+  const [currentZodiac, setCurrentZodiac] = useState(zodiacSign[0]);
   const [zodiacIndex, setZodiacIndex] = useState(0);
+  const oracleQuestionIndex = Math.floor(
+    Math.random() * madameOracleQuestions.length
+  );
+  const [madameOracleQuestion, setMadameOracleQuestion] = useState(
+    madameOracleQuestions[oracleQuestionIndex]
+  );
+
+  // console.log("Zodiac sign in Main: ", zodiacSign);
+  // console.log("Madame Oracle Question in Main: ", madameOracleQuestions);
 
   useEffect(() => {
     const zodiacInterval = setInterval(() => {
       setZodiacIndex((prevIndex) => {
         const newIndex = (prevIndex + 1) % zodiacSign.length;
+
         setCurrentZodiac(zodiacSign[newIndex]);
         return newIndex;
       });
@@ -23,10 +34,13 @@ function Main({ weatherTemp, isDay }) {
 
   return (
     <main className="main">
-      <h1 className="main__title"> Madame Oracle</h1>
-     {currentZodiac.symbol && (
+      <h1 className="main__title">{madameOracleQuestion}</h1>
+      {currentZodiac.url && (
         <Link to="/profile">
-          <div className="zodiac-display">{currentZodiac.name}<br/>{currentZodiac.symbol}</div>
+          <div className="zodiac-display">
+            <br />
+            <img src={currentZodiac.url} className="zodiac__image" />
+          </div>
         </Link>
       )}
     </main>
