a472305c6988fd2378b468aa3658e5b9f9ddd2a1
James Penera <jamespenera@icloud.com>
2024-01-06 20:10:44 -0800

    footer clean up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,17 @@
 import "./Footer.css";
 
-const Footer = ({
-  onAboutUs,
-  onTermsAndConditions,
-}) => {
+const Footer = ({ onAboutUs, onTermsAndConditions }) => {
   return (
     <footer className="footer">
-      <button className="footer__terms_link" type="text" onClick={onTermsAndConditions}>
+      <button
+        className="footer__terms_button"
+        type="text"
+        onClick={onTermsAndConditions}
+      >
         Terms and Conditions
       </button>
       <div className="footer__section">
-        <button className="footer__terms_link" type="text" onClick={onAboutUs}>
+        <button className="footer__terms_button" type="text" onClick={onAboutUs}>
           © 2023 James Penera. All rights reserved.
         </button>
       </div>
