78b838e8143d81aaf3c0ae7b75776583771c2be9
James Penera <jamespenera@icloud.com>
2024-01-12 01:38:07 -0800

    auth

--- src/contexts/CurrentUserContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const CurrentUserContext = React.createContext({
-    // currentUser: {}
-    // setCurrentUSer: () => {},
-});  
-
-export default CurrentUserContext;
